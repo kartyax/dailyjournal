7548044da634bd68fba120da6bf32ac40481c17c
kartyax <kevinartya235@gmail.com>
2026-01-14 15:23:45 +0700

    commit

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['username'])) {
               <div class="text-center mb-3">
                 <div class="text-center mb-3">
                   <i class="bi bi-person-circle h1 display-4"></i>
-                  <p>Welcome to aMy Daily Journal</p>
+                  <p>Welcome to My Daily Journal</p>
                   <hr />
                 </div>
                 <form action="" method="POST" id="loginForm">
--- upload_foto.php
+++ upload_foto.php
@@ -9,16 +9,13 @@ function upload_foto($File){
 	$TmpLocation = $File['tmp_name'];
 	$FileSize = $File['size'];
 
-	//Figure out what kind of file this is:
 	$FileExt = explode('.', $FileName);
 	$FileExt = strtolower(end($FileExt));
 
-	//Allowed files:
 	$Allowed = array('jpg', 'png', 'gif', 'jpeg');  
 
-	// Check file size
-	if ($FileSize > 500000) {
-		$message .= "Sorry, your file is too large, max 500KB. ";
+	if ($FileSize > 2000000) {
+		$message .= "Sorry, your file is too large, max 2MB. ";
 		$uploadOk = 0;
 	}
 
@@ -27,19 +24,14 @@ function upload_foto($File){
 		$message .= "Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
 		$uploadOk = 0; 
 	}
-
-	// Check if $uploadOk is set to 0 by an error
 	if ($uploadOk == 0) {
 		$message .= "Sorry, your file was not uploaded. ";
 		$hasil['status'] = false; 
 	}else{
-				//if everything is ok, try to upload file
-				//Create new filename:
         $NewName = date("YmdHis"). '.' . $FileExt;
         $UploadDestination = "img/". $NewName; 
 
 		if (move_uploaded_file($TmpLocation, $UploadDestination)) {
-			//echo "The file has been uploaded.";
 			$message .= $NewName;
 			$hasil['status'] = true; 
 		}else{
